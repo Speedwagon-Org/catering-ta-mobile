37a064f965841b518df2bac036801fce0cb42ae4
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2023-12-06 23:17:21 +0700

    Added Feature : (+) On Going Order Template (+) Most Popular Around Template (+) Newly Open Restaurant Template
    Need Attention! :
    (?) Fix Distance Measurement

---
+++ app/src/main/java/com/speedwagon/cato/home/menu/adapter/home/NewlyOpenAdapter.kt
@@ -0,0 +1,51 @@
+package com.speedwagon.cato.home.menu.adapter.home
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.speedwagon.cato.R
+import com.speedwagon.cato.home.menu.adapter.home.item.NewlyOpen
+
+class NewlyOpenAdapter (private val context: Context, private val itemList: List<NewlyOpen>) :
+    RecyclerView.Adapter<NewlyOpenAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cardViewContainer: CardView = itemView.findViewById(R.id.cv_item_newly_open_container)
+        val vendorImageView: ImageView = itemView.findViewById(R.id.iv_item_newly_open_image)
+        val vendorNameTextView: TextView = itemView.findViewById(R.id.tv_newly_open_vendor)
+        val vendorDistanceTextView: TextView = itemView.findViewById(R.id.tv_newly_open_distance)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_newly_open, parent, false)
+        return ViewHolder(view)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = itemList[position]
+
+        holder.vendorNameTextView.text = currentItem.vendorName
+        holder.vendorDistanceTextView.text = currentItem.vendorDistance.toString() + " km"
+
+        Glide.with(context)
+            .load(currentItem.vendorImgUrl)
+            .into(holder.vendorImageView)
+
+        holder.cardViewContainer.setOnClickListener {
+            // Handle item click event here
+            // You can pass data or perform an action based on the clicked item
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+
+}
