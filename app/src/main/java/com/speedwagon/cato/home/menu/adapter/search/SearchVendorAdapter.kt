91a350f1e7e9ad349df0e4bdd3922f879d0d6294
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2023-12-10 22:57:48 +0700

    Added Feature : (+) Search Template

---
+++ app/src/main/java/com/speedwagon/cato/home/menu/adapter/search/SearchVendorAdapter.kt
@@ -0,0 +1,54 @@
+package com.speedwagon.cato.home.menu.adapter.search
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.speedwagon.cato.R
+import com.speedwagon.cato.home.menu.adapter.search.item.Vendor
+
+class SearchVendorAdapter (private val context: Context, private val itemList: List<Vendor>)
+    :RecyclerView.Adapter<SearchVendorAdapter.ViewHolder>()
+{
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val searchVendorContainerCardView : CardView = itemView.findViewById(R.id.cv_search_vendor_container)
+        val searchVendorImageImageView : ImageView = itemView.findViewById(R.id.iv_search_vendor_image)
+        val searchVendorNameTextView : TextView = itemView.findViewById(R.id.tv_search_vendor_name)
+        val searchVendorFoodTypeTextView : TextView = itemView.findViewById(R.id.tv_search_vendor_food_type)
+        val searchVendorDistanceTextView : TextView = itemView.findViewById(R.id.tv_search_vendor_distance)
+        val searchVendorBadgeImageView: ImageView = itemView.findViewById(R.id.iv_search_vendor_badge)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_vendor_search, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = itemList[position]
+        holder.searchVendorDistanceTextView.text = currentItem.searchVendorDistance.toString() + " km"
+        holder.searchVendorNameTextView.text = currentItem.searchVendorName
+        holder.searchVendorFoodTypeTextView.text = currentItem.searchVendorType
+        Glide.with(context)
+            .load(currentItem.searchVendorImgUrl)
+            .into(holder.searchVendorImageImageView)
+        holder.searchVendorContainerCardView.setOnClickListener {
+            // Handle item click event here
+            // You can pass data or perform an action based on the clicked item
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/speedwagon/cato/home/menu/adapter/search/item/Vendor.kt
@@ -0,0 +1,9 @@
+package com.speedwagon.cato.home.menu.adapter.search.item
+
+data class Vendor (
+    val searchVendorImgUrl : String,
+    val searchVendorName : String,
+    val searchVendorType : String,
+    val searchVendorDistance : Double,
+    val searchVendorBadge : Int
+)
