6c703ac1332e05bdfb91d67432c35638a3869efe
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2024-01-05 13:53:53 +0700

    Added Feature : (+) Detail Food Template

---
+++ app/src/main/java/com/speedwagon/cato/vendor/foods/DetailFood.kt
@@ -0,0 +1,12 @@
+package com.speedwagon.cato.vendor.foods
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.speedwagon.cato.R
+
+class DetailFood : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_food)
+    }
+}
