c35ade3edb740ee1621853e0504e2df1d38e5971
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2024-01-08 22:04:55 +0700

    Added Feature : (+) Detail Order Template

---
+++ app/src/main/java/com/speedwagon/cato/order/OrderDetail.kt
@@ -0,0 +1,12 @@
+package com.speedwagon.cato.order
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.speedwagon.cato.R
+
+class OrderDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_detail)
+    }
+}
