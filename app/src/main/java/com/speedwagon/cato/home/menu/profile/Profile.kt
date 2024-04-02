15dff31e933d4b1e291a6f3407fe99d4ea8d1958
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2024-04-02 21:53:09 +0700

    Upload Image

--- app/src/main/java/com/speedwagon/cato/home/menu/profile/Profile.kt
+++ app/src/main/java/com/speedwagon/cato/home/menu/profile/Profile.kt
@@ -1,33 +1,54 @@
 package com.speedwagon.cato.home.menu.profile
 
 
+import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.MimeTypeMap
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.speedwagon.cato.R
 import com.speedwagon.cato.auth.Authentication
 import com.speedwagon.cato.home.menu.profile.information.Information
 import com.speedwagon.cato.home.menu.profile.location.Location
+import java.util.UUID
 
 
 class Profile : Fragment() {
+
+    private val PICK_IMAGE_REQUEST = 1
+
     private lateinit var profileImg : ImageView
     private lateinit var username : TextView
     private lateinit var email : TextView
     private lateinit var phone : TextView
     private lateinit var logoutBtn : CardView
     private lateinit var infoMenu : CardView
+
+    private lateinit var storage : FirebaseStorage
+    private lateinit var storageReference : StorageReference
+    private lateinit var auth : FirebaseAuth
+    private lateinit var db : FirebaseFirestore
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,10 +60,13 @@ class Profile : Fragment() {
         phone = view.findViewById(R.id.tv_profile_phone_number)
         logoutBtn = view.findViewById(R.id.tv_profile_logout)
         infoMenu = view.findViewById(R.id.cv_profile_option_customer_information)
-        val auth = FirebaseAuth.getInstance()
+
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
         val uid = auth.uid
         val customerRef = FirebaseFirestore.getInstance().collection("customer")
-
+        storage = FirebaseStorage.getInstance()
+        storageReference = storage.reference
         if (uid != null){
             customerRef.document(uid).get().addOnCompleteListener {task ->
                 if (task.isSuccessful){
@@ -52,7 +76,7 @@ class Profile : Fragment() {
                         email.text = data.get("email") as String
                         phone.text = data.get("phone") as String
                         val profileImgPath = data.get("profile_picture") as String
-                        val storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(profileImgPath)
+                        val storageReference = storage.getReferenceFromUrl(profileImgPath)
 
                         storageReference.downloadUrl.addOnSuccessListener { uri ->
                             Glide.with(requireContext())
@@ -83,6 +107,33 @@ class Profile : Fragment() {
                 .show()
         }
 
+
+        profileImg.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.READ_MEDIA_IMAGES),
+                    PICK_IMAGE_REQUEST
+                )
+            } else if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PICK_IMAGE_REQUEST
+                )
+            }
+            else {
+                openGallery()
+            }
+        }
         val locationButton = view.findViewById<CardView>(R.id.cv_profile_option_location)
         locationButton.setOnClickListener{
             val intent = Intent(context, Location::class.java)
@@ -91,4 +142,68 @@ class Profile : Fragment() {
         return view
     }
 
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            val imageUri: Uri = data.data!!
+            uploadImage(imageUri)
+        }
+    }
+
+    private fun uploadImage(imageUri: Uri) {
+        val fileExtension = MimeTypeMap.getSingleton().getExtensionFromMimeType(requireContext().contentResolver.getType(imageUri))
+        val imageName = "${UUID.randomUUID()}.$fileExtension"
+        val imageRef = storageReference.child("${auth.currentUser!!.uid}/$imageName")
+
+        imageRef.putFile(imageUri)
+            .addOnSuccessListener { _ ->
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    val imageUrl = uri.toString()
+
+                    // Update the profile picture URL in Firestore
+                    db.collection("customer").document(auth.currentUser!!.uid)
+                        .update("profile_picture", imageUrl)
+                        .addOnSuccessListener {
+                            // Profile picture URL updated successfully
+                            Toast.makeText(context, "Gambar telah diupdate", Toast.LENGTH_SHORT)
+                                .show()
+                            // Load the image using Glide into profileImg
+                            Glide.with(requireContext())
+                                .load(imageUrl)
+                                .into(profileImg)
+                        }
+                        .addOnFailureListener { e ->
+                            // Handle error updating profile picture URL
+                            Log.e(TAG, "Error updating profile picture URL: $e")
+                        }
+                }
+            }
+            .addOnFailureListener { e ->
+                // Handle image upload failure
+                Log.e(TAG, "Error uploading image: $e")
+                // You may want to show a Toast or dialog to inform the user about the error
+            }
+    }
+
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PICK_IMAGE_REQUEST) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery()
+            }
+        }
+    }
 }
